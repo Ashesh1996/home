48befd0abc9ece4a6ef6c5154ca63a638a2c952f
Ashesh Roy <59134661+Ashesh1996@users.noreply.github.com>
2020-07-19 16:15:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-        if(this.scrollY > 20)
-        $(".navbar").addClass("sticky");
-        else
-        $(".navbar").removeClass("sticky");
+        if(this.scrollY > 20){
+            $(".navbar").addClass("sticky");  
+            $(".goup").show();
+        }
+        else{
+            $(".navbar").removeClass("sticky");
+            $(".goup").hide();
+        }
     });
+    $(".goup").click(function(){
+        scroll(0,0)
+    });
+    $(".know").click(function(){
+        
+        $('html, body').animate({
+            scrollTop: ($('#about').offset().top)
+        },500);
+    });
+   
+
     $(".menu-toggler").click(function(){
         $(this).toggleClass("active");
         $(".navbar-menu").toggleClass("active");
